d8b26ceaeee6cc61af9a88a867ddcf2d497c8d75
Ivan <ivan.m.sidorchuk@gmail.com>
2023-10-22 18:29:21 -0700

    just push it

--- script.groovy
+++ script.groovy
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-=======
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
-}
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
-    }
-}
-
->>>>>>> 468634f64a79a2196ae9816490366626a815ec6e
 def deployApp() {
     echo "Deploying the application"
 }
