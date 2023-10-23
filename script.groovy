1919807e1e246660f4d1d9730745cb7257f6bb34
Ivan <ivan.m.sidorchuk@gmail.com>
2023-10-22 18:20:16 -0700

    implementing shared lib in Jf

--- script.groovy
+++ script.groovy
@@ -3,20 +3,6 @@ def test() {
     echo "Executing pipeleine on branch: ${BRANCH_NAME}"
 }
 
-def buildJar() {
-    echo "Building the jar file"
-    sh "mvn package"
-}
-
-def buildImage() {
-    echo "Building the docker image"
-    withCredentials([usernamePassword(credentialsId: 'devops-token', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t $USER/devops:jma-2.0 ."
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push $USER/devops:jma-2.0"
-    }
-}
-
 def deployApp() {
     echo "Deploying the application"
 }
